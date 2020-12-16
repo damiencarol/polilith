24a0cf64451afeae0d737a90357489ebe5f5ad6c
Damien Carol <damien.carol@gmail.com>
2020-12-16 17:29:00 +0100

    Improve argument parsing and report generation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-use std::path::Path;
+use clap::{App, Arg};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
-use tar::Archive;
-use serde::{Serialize, Deserialize};
 use std::io::prelude::*;
+use std::path::Path;
+use tar::Archive;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct SarifLog {
+    #[serde(rename = "$schema")]
+    schema: String,
     version: String,
-    runs: Vec<Run>
+    runs: Vec<Run>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Run {
     tool: Tool,
-    artifacts:  Vec<Artifact>,
-    results: Vec<Result>
+    artifacts: Vec<Artifact>,
+    results: Vec<Result>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Tool {
-    driver: Driver
+    driver: Driver,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,41 +31,39 @@ struct Driver {
     name: String,
     information_uri: String,
     full_name: String,
-    version: String
+    version: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Artifact {
-    location: ArtifactLocation
+    location: ArtifactLocation,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ArtifactLocation {
-    uri: String
+    uri: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Result {
     rule_id: String,
-    message: ResultMessage
+    message: ResultMessage,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ResultMessage {
-    text: String
+    text: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ReportingDescriptor {
-    message: String
+    message: String,
 }
-
 
 fn rule_pl001(ar: &mut Archive<File>) -> Vec<Result> {
     let rule_id = "PL001".to_string();
     let text = "User root pb!".to_string();
-    
     //let file = ar.unpack("manifest.json").unwrap();
     //let file = ar.unpack("manifest.json");//.unwrap();
     // get the manifest
@@ -98,39 +99,89 @@ fn rule_pl001(ar: &mut Archive<File>) -> Vec<Result> {
         }
     }
 
-    vec![Result{ 
+    vec![Result {
         rule_id: rule_id,
-        message: ResultMessage{ text: text }
+        message: ResultMessage { text: text },
     }]
 }
 
-
-fn main() -> std::io::Result<()> {
-    let image_name = "busybox:latest";
-    let mut ar = Archive::new(File::open("busybox-latest.tar").unwrap());
+fn analyze_one_archive(driver: Driver, input: &str) -> SarifLog {
+    let mut ar = Archive::new(File::open(input).unwrap());
 
     // create a run
-    let tool = Tool{ driver: Driver{ 
-        name: "polilith".to_string(),
-        information_uri: "".to_string(),
-        full_name: "Polilith Docker image quality tool".to_string(),
-        version: "0.0.1".to_string()
-    } };
-    let mut run = Run{ tool : tool, artifacts: Vec::new(), results: Vec::new() };
+    let tool = Tool { driver: driver };
+    let mut run = Run {
+        tool: tool,
+        artifacts: Vec::new(),
+        results: Vec::new(),
+    };
     // add docker image as artifact
-    run.artifacts.push(Artifact{
-        location: ArtifactLocation{ uri: image_name.to_string() }
+    run.artifacts.push(Artifact {
+        location: ArtifactLocation { uri: (&input).to_string() },
     });
     // test PL001 and aggregate results
     run.results.extend(rule_pl001(&mut ar));
 
     // create a report with only one run
-    let mut log = SarifLog{ version : "2.1.0".to_string(), runs : Vec::new()};
+    let mut log = SarifLog {
+        schema: "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json".to_string(),
+        version: "2.1.0".to_string(),
+        runs: Vec::new(),
+    };
     log.runs.push(run);
-    
-    // export as a SARIF file
-    let mut file = File::create("report.sarif")?;
-    let json = serde_json::to_string_pretty(&log).unwrap();
-    file.write_all(json.as_bytes())?;
+
+    log
+}
+
+fn main() -> std::io::Result<()> {
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let author = "Damien Carol <damien.carol@gmail.com>";
+    let about = env!("CARGO_PKG_DESCRIPTION");
+    let uri = env!("CARGO_PKG_HOMEPAGE");
+    // parse command line
+    let args = App::new(name)
+        .version(version)
+        .author(author)
+        .about(about)
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .takes_value(true)
+                .help("Docker image file"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("out")
+                .takes_value(true)
+                .help("Report file"),
+        );
+    let matches = args.get_matches();
+
+    let myfile = matches
+        .value_of("file")
+        .unwrap_or("busybox-latest.tar")
+        .to_string();
+
+    // boostrap context
+    let driver = Driver {
+        name: name.to_string(),
+        information_uri: uri.to_string(),
+        full_name: about.to_string(),
+        version: version.to_string(),
+    };
+    // do one run
+    println!("Analysing file {}...", myfile);
+    let log = analyze_one_archive(driver, &myfile);
+
+    // manage ouput
+    if matches.is_present("output") {
+        // export as a SARIF file
+        let mut file = File::create(matches.value_of("output").unwrap())?;
+        let json = serde_json::to_string_pretty(&log).unwrap();
+        file.write_all(json.as_bytes())?;
+    }
     Ok(())
 }
