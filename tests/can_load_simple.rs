c6c49a21bcf99db3745cd487411d802a3718e84f
Damien Carol <damien.carol@gmail.com>
2025-07-12 07:27:21 +0200

    Fix test in can_load_simple.rs

--- tests/can_load_simple.rs
+++ tests/can_load_simple.rs
@@ -10,7 +10,7 @@ pub fn can_load_simple_image() {
     let mut ar = Archive::new(File::open("busybox-1.32.0.tar").unwrap());
     let manifest = docker::get_manifest(&mut ar);
     assert_eq!(
-        "219ee5171f8006d1462fa76c12b9b01ab672dbc8b283f186841bf2c3ca8e3c93.json",
+        "blobs/sha256/219ee5171f8006d1462fa76c12b9b01ab672dbc8b283f186841bf2c3ca8e3c93",
         manifest[0].config
     )
 }
